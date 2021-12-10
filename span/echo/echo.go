933dfb23ad19c2eb5909e6392c5c2ea3d9afed26
Irvan Maulana <me@tukangremot.com>
2021-12-10 19:25:52 +0700

    refactor: root source directory

--- span/echo/echo.go
+++ span/echo/echo.go
@@ -3,7 +3,7 @@ package echo
 import (
 	"github.com/getsentry/sentry-go"
 	"github.com/labstack/echo/v4"
-	span "github.com/tukangremot/go-sentry/src/span"
+	span "github.com/tukangremot/go-sentry/span"
 )
 
 func Start(context echo.Context) *sentry.Span {
