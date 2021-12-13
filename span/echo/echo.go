785131a09096c481dea16f7a160e761b1293387e
Irvan Maulana <me@tukangremot.com>
2021-12-13 14:49:55 +0700

    fix: span for echo

--- span/echo/echo.go
+++ span/echo/echo.go
@@ -1,11 +1,17 @@
 package echo
 
 import (
+	"runtime"
+
 	"github.com/getsentry/sentry-go"
 	"github.com/labstack/echo/v4"
-	span "github.com/tukangremot/go-sentry/span"
 )
 
 func Start(context echo.Context) *sentry.Span {
-	return span.Start(context.Request().Context(), sentry.TransactionName(context.Request().URL.Path))
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+
+	return sentry.StartSpan(context.Request().Context(), frame.Function, sentry.TransactionName(context.Request().URL.Path))
 }
